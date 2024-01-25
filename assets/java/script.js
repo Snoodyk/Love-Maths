06feba5164dc85e5c06e4b4153bbd5927fed9d3d
Nathan Snoodyk <nathansnoodyk@gmail.com>
2024-01-25 14:02:58 +0000

    Fixed missing rungame javascript

--- assets/java/script.js
+++ assets/java/script.js
@@ -12,17 +12,20 @@ document.addEventListener("DOMContentLoaded", function(){
 
         })
     }
+    runGame("addition");
 })
 
 
 function runGame(gameType){
+    
     let num1= Math.floor(Math.random() * 25) + 1;
     let num2= Math.floor(Math.random() * 25) + 1;
+    
     if(gametype === "addition"){
         displayAdditionQuestion(num1,num2)
     } else {
         alert(`unknown game type: ${gameType}`);
-        throw`unknown game type: ${gameType}.Ablorting!`;
+        throw `unknown game type: ${gameType}.Ablorting!`;
     }
 }
 
@@ -44,9 +47,9 @@ function incrementWrongScore(){
 }
 
 function displayAdditionQuestion(operand1,operand2){
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
-    document.getElementById("operator").textContent = "+";
+    document.getElementById(`operand1`).textContent = operand1;
+    document.getElementById(`operand2`).textContent = operand2;
+    document.getElementById(`operator`).textContent = "+";
 }
 
 function displaySubtractQuestion(){
