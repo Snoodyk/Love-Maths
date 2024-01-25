7fd5a3faf82fc4e49fbd2281977db1ce83535993
Nathan Snoodyk <nathansnoodyk@gmail.com>
2024-01-25 13:49:54 +0000

    Added start of javascript to additions

--- assets/java/script.js
+++ assets/java/script.js
@@ -7,16 +7,23 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked Submit!");
             }else{
                     let gameType= this.getAttribute("data-type");
-                    alert(`You clicked ${gameType}`);
+                    runGame(gameType)
             }
 
         })
     }
 })
 
-function runGame(){
+
+function runGame(gameType){
     let num1= Math.floor(Math.random() * 25) + 1;
     let num2= Math.floor(Math.random() * 25) + 1;
+    if(gametype === "addition"){
+        displayAdditionQuestion(num1,num2)
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw`unknown game type: ${gameType}.Ablorting!`;
+    }
 }
 
 
@@ -36,8 +43,10 @@ function incrementWrongScore(){
 
 }
 
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1,operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion(){
