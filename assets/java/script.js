3a5bac5935c7a9ad589c52b46ff7cd98fdfe3923
Nathan Snoodyk <nathansnoodyk@gmail.com>
2024-01-26 14:28:01 +0000

    Added increment scores to the game

--- assets/java/script.js
+++ assets/java/script.js
@@ -36,8 +36,10 @@ function checkAnswer(){
     let isCorrect = userAnswer === calculatedAnswer [0];
     if (isCorrect) {
         alert ("Hey! You got it right! :)")
+        incrementScore();
     } else {
         alert (`Awww..... You answered ${userAnswer}. The correct answer was ${calculatedAnswer}! :(`)
+        incrementWrongScore();
     }
     
 }
@@ -58,12 +60,14 @@ if (operator ==="+"){
 }
 
 function incrementScore(){
-
+let oldScore = parseInt(document.getElementById("score").innerText);
+document.getElementById("score").innerText= ++oldScore;
 }
 
 function incrementWrongScore(){
-
-}
+        let oldScore = parseInt(document.getElementById("incorrect").innerText);
+        document.getElementById("incorrect").innerText= ++oldScore;
+        }
 
 function displayAdditionQuestion(operand1,operand2){
     document.getElementById(`operand1`).textContent = operand1;
