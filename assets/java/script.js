cda215057443a8ab38ca6f94e3d93ce27144be5c
Nathan Snoodyk <nathansnoodyk@gmail.com>
2024-01-26 14:56:51 +0000

    Added subtraction game code

--- assets/java/script.js
+++ assets/java/script.js
@@ -29,7 +29,9 @@ function runGame(gameType){
         else if (gameType === "division"){
             displayDivideQuestion(num1,num2)
         }
-    
+        else if (gameType === "subtract"){
+            displaySubtractQuestion(num1,num2)
+        }
         else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}.Aborting!`;
@@ -63,6 +65,9 @@ if (operator ==="+"){
     else if (operator === "x"){
         return [operand1 * operand2, "multiply"]
     }
+    else if (operator === "-"){
+        return [operand1 - operand2, "subtract"]
+    }
 
     else {
     alert(`Unimplemented operator ${operator}`);
@@ -87,8 +92,10 @@ function displayAdditionQuestion(operand1,operand2){
     document.getElementById(`operator`).textContent = "+";
 }
 
-function displaySubtractQuestion(){
-
+function displaySubtractQuestion(operand1,operand2){
+    document.getElementById(`operand1`).textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById(`operand2`).textContent = operand1 > operand2 ? operand1 : operand1;
+    document.getElementById(`operator`).textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1,operand2){
