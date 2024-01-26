70aae8b0d201f378bcdd9fa6007772dfacc625ce
Nathan Snoodyk <nathansnoodyk@gmail.com>
2024-01-26 14:41:40 +0000

    Added multiplication game code

--- assets/java/script.js
+++ assets/java/script.js
@@ -23,7 +23,14 @@ function runGame(gameType){
     
     if(gameType === "addition"){
         displayAdditionQuestion(num1,num2)
-    } else {
+    }   else if (gameType === "multiply"){
+        displayMultiplyQuestion(num1,num2)
+    }
+        else if (gameType === "division"){
+            displayDivideQuestion(num1,num2)
+        }
+    
+        else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}.Aborting!`;
     }
@@ -52,7 +59,12 @@ let operator = document.getElementById(`operator`).innerText;
 
 if (operator ==="+"){
     return [operand1 + operand2]//,"addition"];
-} else {
+}   
+    else if (operator === "x"){
+        return [operand1 * operand2, "multiply"]
+    }
+
+    else {
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Abourting`;
 }
@@ -79,8 +91,10 @@ function displaySubtractQuestion(){
 
 }
 
-function displayMultiplyQuestion(){
-
+function displayMultiplyQuestion(operand1,operand2){
+    document.getElementById(`operand1`).textContent = operand1;
+    document.getElementById(`operand2`).textContent = operand2;
+    document.getElementById(`operator`).textContent = "x";
 }
 
 function displayDivideQuestion(){
