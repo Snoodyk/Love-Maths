67c13e502be627d27f75011521858844d4696700
Nathan Snoodyk <nathansnoodyk@gmail.com>
2024-01-29 18:22:04 +0000

    fixed spelling of division

--- assets/java/script.js
+++ assets/java/script.js
@@ -44,7 +44,7 @@ function runGame(gameType){
         }
         
         else if (gameType === "division"){
-            displayDivideQuestion(num1,num2)
+            displayDivisionQuestion(num1,num2)
         }
         else {
         alert(`unknown game type: ${gameType}`);
@@ -122,7 +122,7 @@ function displayMultiplyQuestion(operand1,operand2){
     document.getElementById(`operator`).textContent = "x";
 }
 
-function displayDivideQuestion(operand1,operand2){
+function displayDivisionQuestion(operand1,operand2){
     document.getElementById('operand1').textContent = operand1 * operand2
     document.getElementById('operand2').textContent = operand1 > operand2 ? operand1 : operand1;
     document.getElementById('operator').textContent = '/';
