05885122a367382fe2996a87521cb3ff0fec06a1
Nathan Snoodyk <nathansnoodyk@gmail.com>
2024-01-29 18:09:04 +0000

    Added division code

--- assets/java/script.js
+++ assets/java/script.js
@@ -85,7 +85,7 @@ if (operator ==="+"){
         return [operand1 - operand2, "subtract"]
     }
     else if (operator === "/"){
-        return [operand1 - operand2, "division"]
+        return [operand1 / operand2, "division"]
     }
     else {
     alert(`Unimplemented operator ${operator}`);
@@ -123,5 +123,7 @@ function displayMultiplyQuestion(operand1,operand2){
 }
 
 function displayDivideQuestion(operand1,operand2){
-  
+    document.getElementById('operand1').textContent = operand1 * operand2
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand1 : operand1;
+    document.getElementById('operator').textContent = '/';
 }
